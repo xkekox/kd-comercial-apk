d8f81bdaf06930a2f1890ca95e4e95236a96ac83
xkekox <jefersonkeko15@gmail.com>
2026-03-18 08:46:46 -0300

    Refresh service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kd-comercial-v1";
+const CACHE_NAME = "kd-comercial-v2";
 const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
