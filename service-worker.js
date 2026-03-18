75fcf72d33451f9c82befa1b5eee904aaa985a75
xkekox <jefersonkeko15@gmail.com>
2026-03-18 11:48:36 -0300

    Sync root service worker from web

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kd-comercial-v3";
+const CACHE_NAME = "kd-comercial-v4";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
